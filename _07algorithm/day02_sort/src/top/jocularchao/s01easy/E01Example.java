8dbc015d70ca266987cdf3eec90f8875ba0ca88b
jocularchao <jocularchao@163.com>
2023-06-29 11:36:01 +0800

    众多排序的模板

--- _07algorithm/day02_sort/src/top/jocularchao/s01easy/E01Example.java
+++ _07algorithm/day02_sort/src/top/jocularchao/s01easy/E01Example.java
@@ -9,4 +9,34 @@ package top.jocularchao.s01easy;
  * @description:
  */
 public class E01Example {
+    public void sort(Comparable[] a){
+
+    }
+
+    public boolean less(Comparable v,Comparable w){
+        return v.compareTo(w)<0;
+    }
+    public void exch(Comparable[]a,int i ,int j){
+        Comparable t = a[i];
+        a[i]=a[j];
+        a[j]= t;
+    }
+
+    public void show(Comparable[] a){
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public boolean isSorted(Comparable[] a){
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i],a[i-1])){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 }
