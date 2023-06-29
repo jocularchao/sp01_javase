cf20689dda99db2099bdb9a90ec7d338461e15bb
jocularchao <jocularchao@163.com>
2023-06-29 16:22:53 +0800

    排序模板

--- _07algorithm/day02_sort/src/top/jocularchao/s01easy/E01Example.java
+++ _07algorithm/day02_sort/src/top/jocularchao/s01easy/E01Example.java
@@ -5,30 +5,30 @@ package top.jocularchao.s01easy;
  *
  * @author JocularChao
  * @date 2023/6/28 14:53
- * @Description
+ * @Description 排序模板
  */
 public class E01Example {
-    public void sort(Comparable[] a){
+    public static void sort(Comparable[] a){
 
     }
 
-    public boolean less(Comparable v,Comparable w){
+    public static boolean less(Comparable v,Comparable w){
         return v.compareTo(w)<0;
     }
-    public void exch(Comparable[]a,int i ,int j){
+    public static void exch(Comparable[]a,int i ,int j){
         Comparable t = a[i];
         a[i]=a[j];
         a[j]= t;
     }
 
-    public void show(Comparable[] a){
+    public static void show(Comparable[] a){
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i]+" ");
         }
         System.out.println();
     }
 
-    public boolean isSorted(Comparable[] a){
+    public static boolean isSorted(Comparable[] a){
         for (int i = 1; i < a.length; i++) {
             if (less(a[i],a[i-1])){
                 return false;
@@ -37,5 +37,11 @@ public class E01Example {
         return true;
     }
 
+    public static void main(String[] args) {
+        Integer a[] = {1,3,2};
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
 
 }
