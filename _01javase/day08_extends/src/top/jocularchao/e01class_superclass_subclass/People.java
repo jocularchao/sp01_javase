0739e4209e76bf68641e80ca18db2751d00262c9
jocularchao <jocularchao@163.com>
2023-07-15 18:45:16 +0800

    父类

--- _01javase/day08_extends/src/top/jocularchao/e01class_superclass_subclass/People.java
+++ _01javase/day08_extends/src/top/jocularchao/e01class_superclass_subclass/People.java
@@ -4,6 +4,13 @@ public class People {
     private String name;
     private int age;
 
+    public People() {
+    }
+
+    public People(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
     public String getName() {
         return name;
