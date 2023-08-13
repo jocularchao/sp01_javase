bca8ad76b3144b522e68c1e9eb52a037b61cb764
jocularchao <jocularchao@163.com>
2023-08-13 19:24:34 +0800

    DML DDL

--- _01javase/day17_jdbc/src/top/jocularchao/JDBC02DML.java
+++ _01javase/day17_jdbc/src/top/jocularchao/JDBC02DML.java
@@ -17,8 +17,10 @@ public class JDBC02DML {
         try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc_test", "root", "123456");
              Statement statement = connection.createStatement()
         ) {
-            int i = statement.executeUpdate("insert into `student` values (20230812,'十二',25)");
+            //int i = statement.executeUpdate("insert into `student` values (20230812,'十二',25)");
 
+            //int i = statement.executeUpdate("update student set name = '十三' where id = 20230812");
+            int i = statement.executeUpdate("truncate table student;");
             System.out.println("生效了："+i+"行");
         } catch (SQLException e) {
             throw new RuntimeException(e);
