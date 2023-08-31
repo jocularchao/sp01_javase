5fb8683bf44b4cbbf575a1c0288726b779e8c56c
jocularchao <jocularchao@gmail.com>
2023-08-31 19:31:03 +0800

    自动类型转换

---
+++ _01javase/day01_Introduction/src/chao/a07type/TypeDemo1.java
@@ -0,0 +1,11 @@
+package chao.a07type;
+
+/**
+ * Create with IntelliJ IDEA.
+ *
+ * @author JocularChao
+ * @date 2023/8/31 19:30
+ * @Description 理解自动类型转换   类型范围小的变量可以直接赋值给类型范围大的变量
+ */
+public class TypeDemo1 {
+}
