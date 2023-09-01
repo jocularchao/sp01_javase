8a41b2e314e20f5e68222629de51735de0ed346d
jocularchao <jocularchao@gmail.com>
2023-09-01 17:34:07 +0800

    三元运算符

---
+++ _01javase/day01_Introduction/src/chao/a06operator/Operator02Ternary.java
@@ -0,0 +1,12 @@
+package chao.a06operator;
+
+/**
+ * Create with IntelliJ IDEA.
+ *
+ * @author JocularChao
+ * @date 2023/9/1 17:33
+ * @Description 三元运算符
+ */
+public class Operator02Ternary {
+
+}
