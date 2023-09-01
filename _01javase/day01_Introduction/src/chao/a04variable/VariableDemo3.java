cdf27ea36c240763bb6e845cd5b00250bd818b25
jocularchao <jocularchao@gmail.com>
2023-09-01 17:50:39 +0800

    BigDecimal 和  BigInteger

---
+++ _01javase/day01_Introduction/src/chao/a04variable/VariableDemo3.java
@@ -0,0 +1,14 @@
+package chao.a04variable;
+
+/**
+ * Create with IntelliJ IDEA.
+ *
+ * @author JocularChao
+ * @date 2023/9/1 17:50
+ * @Description 如果基本的整数和浮点数精度布恩那个满足需求，那么可以使用可以使用这俩各类：
+ * BigDecimal 和  BigInteger
+ * 任意精度浮点数 和 任意精度整数
+ */
+public class VariableDemo3 {
+
+}
