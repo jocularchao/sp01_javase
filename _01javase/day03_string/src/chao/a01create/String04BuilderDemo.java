c022df6eea78b5a50b4cfec7e89a16f537cb9a71
jocularchao <jocularchao@gmail.com>
2023-09-02 12:13:04 +0800

    StringBuilder

--- _01javase/day03_string/src/chao/a01create/String04BuilderDemo.java
+++ _01javase/day03_string/src/chao/a01create/String04BuilderDemo.java
@@ -7,7 +7,6 @@ package chao.a01create;
  * @E-mail: jocularchao@163.com
  * @Date: 2023/4/23 20:28
  * @description:
- *
  * 当需要由较短的字符串构建字符串时，由于每次连接字符串都会构建一个新的String对象。既耗时，又浪费空间。
  */
 public class String04BuilderDemo {
