6c8a3e1fb0955c7cf87ac37f89a9c5bddcd07ba5
jocularchao <jocularchao@gmail.com>
2023-09-03 20:49:46 +0800

    静态代码块

---
+++ _01javase/day05_class_method/src/c02static_final/StaticBlock.java
@@ -0,0 +1,14 @@
+package c02static_final;
+
+/**
+ * Create with IntelliJ IDEA.
+ *
+ * @author JocularChao
+ * @date 2023/9/3 20:44
+ * @Description 静态代码块
+ */
+public class StaticBlock {
+    static {
+        
+    }
+}
