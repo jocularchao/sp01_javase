712945a5bec4969ecd99e08fd4cd433b420ade13
jocularchao <jocularchao@gmail.com>
2023-09-05 10:57:00 +0800

    子类 super、this的使用

--- _01javase/day06_extends/src/e02super_this/Sub.java
+++ _01javase/day06_extends/src/e02super_this/Sub.java
@@ -5,9 +5,22 @@ package e02super_this;
  *
  * @author JocularChao
  * @date 2023/9/5 10:34
- * @Description 子类 super、this的使用
+ * @Description 子类
+ *
+ * super、this的使用
+ * 1、2、3以期使其屏蔽的方法或变量变为可见
+ * 4 在构造方法中，调用父类的构造方法
  */
 public class Sub extends Base{
+
+    public Sub(String var) {
+        super(var);
+    }
+
+    public Sub(String varBase, String var) {
+        super(varBase);
+        this.var = var;
+    }
 
     //3 子类定义了和父类同名的成员变量
     String var = "SubVar";
@@ -35,7 +48,7 @@ public class Sub extends Base{
 
 
     public static void main(String[] args) {
-        Sub sub = new Sub();
+        Sub sub = new Sub("baseVar");
 
         sub.test();
     }
