073847e4b776f8ba1b44f6a5391f1ec3fdb824ee
jocularchao <jocularchao@gmail.com>
2023-09-05 10:16:44 +0800

    抽象类做父类

---
+++ _01javase/day06_extends/src/e01base/Sub02Abstract.java
@@ -0,0 +1,12 @@
+package e01base;
+
+/**
+ * Create with IntelliJ IDEA.
+ *
+ * @author JocularChao
+ * @date 2023/9/4 21:55
+ * @Description 抽象方法如何重写、抽象方法如何重写为抽象方法
+ */
+public class Sub02Abstract {
+
+}
