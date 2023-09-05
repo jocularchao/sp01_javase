0c84e004964b6b931f391bdab895f9ecd088e756
jocularchao <jocularchao@gmail.com>
2023-09-05 10:52:38 +0800

    子类 super、this的使用

---
+++ _01javase/day06_extends/src/e02super_this/Sub.java
@@ -0,0 +1,42 @@
+package e02super_this;
+
+/**
+ * Create with IntelliJ IDEA.
+ *
+ * @author JocularChao
+ * @date 2023/9/5 10:34
+ * @Description 子类 super、this的使用
+ */
+public class Sub extends Base{
+
+    //3 子类定义了和父类同名的成员变量
+    String var = "SubVar";
+
+
+    //2 子类的某个方法重写了父类的一个方法
+    @Override
+    void BaseMethod() {
+        System.out.println("SubMethod");
+    }
+
+    void test(){
+        //1 一个方法内，当局部变量和类的成员变量同名
+        String var = "testVar";
+
+        //使用super、this
+        System.out.println(var);
+        System.out.println(this.var);
+        System.out.println(super.var);
+
+        BaseMethod();
+        this.BaseMethod();
+        super.BaseMethod();
+    }
+
+
+    public static void main(String[] args) {
+        Sub sub = new Sub();
+
+        sub.test();
+    }
+}
