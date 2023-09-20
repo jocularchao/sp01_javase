3a937d4f27189cf6e88354a95f98ba1b8ad952ba
jocularchao <jocularchao@gmail.com>
2023-09-20 23:38:14 +0800

    链表

---
+++ _01javase/day12_list/src/top/jocularchao/l01start/LinkedListTest.java
@@ -0,0 +1,18 @@
+package top.jocularchao.l01start;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Create with IntelliJ IDEA.
+ *
+ * @author JocularChao
+ * @date 2023/9/20 14:30
+ * @Description 链表
+ */
+public class LinkedListTest {
+
+    public static void main(String[] args) {
+        List<String> list = new LinkedList<String>();
+    }
+}
