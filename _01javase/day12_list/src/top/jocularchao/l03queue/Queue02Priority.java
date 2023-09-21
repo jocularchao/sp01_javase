a4b9f594000b8329671b973189585ee6a12ca3fc
jocularchao <jocularchao@gmail.com>
2023-09-21 18:45:40 +0800

    实现优先级队列

---
+++ _01javase/day12_list/src/top/jocularchao/l03queue/Queue02Priority.java
@@ -0,0 +1,20 @@
+package top.jocularchao.l03queue;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * Create with IntelliJ IDEA.
+ *
+ * @author JocularChao
+ * @date 2023/9/21 18:43
+ * @Description 优先级队列
+ */
+public class Queue02Priority {
+    public static void main(String[] args) {
+        //PriorityQueue来实现优先级队列
+        Queue<Object> queue = new PriorityQueue<>();
+
+
+    }
+}
