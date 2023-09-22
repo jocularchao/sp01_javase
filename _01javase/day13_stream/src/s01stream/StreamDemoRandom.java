0900bf4b5b59dca44f842432a30c32e5fa197efa
jocularchao <jocularchao@gmail.com>
2023-09-22 23:12:18 +0800

    random生成的流

---
+++ _01javase/day13_stream/src/s01stream/StreamDemoRandom.java
@@ -0,0 +1,16 @@
+package s01stream;
+
+/**
+ * Create with IntelliJ IDEA.
+ *
+ * @author JocularChao
+ * @date 2023/9/22 23:11
+ * @Description random生成的流
+ */
+public class StreamDemoRandom {
+    public static void main(String[] args) {
+        // random支持直接生成随机数的流
+
+
+    }
+}
