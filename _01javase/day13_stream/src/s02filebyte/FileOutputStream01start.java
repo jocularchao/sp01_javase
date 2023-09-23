7419d873d61c2545437599881909fa2767878416
jocularchao <jocularchao@gmail.com>
2023-09-23 17:31:50 +0800

    FileOutputStream文件字节输出流

--- _01javase/day13_stream/src/s02filebyte/FileOutputStream01start.java
+++ _01javase/day13_stream/src/s02filebyte/FileOutputStream01start.java
@@ -1,5 +1,8 @@
 package s02filebyte;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * Create with IntelliJ IDEA.
  *
@@ -8,4 +11,12 @@ package s02filebyte;
  * @Description FileOutputStream文件字节输出流
  */
 public class FileOutputStream01start {
+    public static void main(String[] args) {
+        try(FileOutputStream outputStream =
+                    new FileOutputStream("./day13_stream/fileoutput.txt")) {
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
