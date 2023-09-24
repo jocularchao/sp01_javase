2c007112c41a02148beeb06812ad6a7df650a228
jocularchao <jocularchao@gmail.com>
2023-09-24 20:40:18 +0800

    BufferedOutputStream

---
+++ _01javase/day13_stream/src/s04buffer/Buffer02OutputStream.java
@@ -0,0 +1,12 @@
+package s04buffer;
+
+/**
+ * Create with IntelliJ IDEA.
+ *
+ * @author JocularChao
+ * @date 2023/9/24 20:38
+ * @Description BufferedOutputStream
+ * 操作和FileOutputStream一致
+ */
+public class Buffer02OutputStream {
+}
