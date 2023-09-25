e68ce049f03972a16723ba5e46d523cd3a2c7d5c
jocularchao <jocularchao@gmail.com>
2023-09-25 18:48:39 +0800

    delete

---
+++ _01javase/day14_thread/src/t02status/Thread04Suspend.java
@@ -0,0 +1,29 @@
+package t02status;
+
+
+/**
+ * Create with IntelliJ IDEA.
+ *
+ * @author JocularChao
+ * @date 2023/9/25 18:43
+ * @Description 暂停线程
+ */
+public class Thread04Suspend {
+    public static void main(String[] args) {
+        Thread t = new Thread(() ->{
+            System.out.println("thread start");
+
+            Thread.currentThread().suspend(); //暂停
+            System.out.println("线程继续执行");
+        });
+
+        t.start();
+
+        try {
+            Thread.sleep(5000);
+            t.resume();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
