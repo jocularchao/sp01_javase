318bcc5580c02efdccf61c8ff14f97e173c479c4
jocularchao <jocularchao@gmail.com>
2023-09-25 12:56:06 +0800

    线程状态

---
+++ _01javase/day14_thread/src/t02status/Thread03Interrupt.java
@@ -0,0 +1,16 @@
+package t02status;
+
+/**
+ * Create with IntelliJ IDEA.
+ *
+ * @author JocularChao
+ * @date 2023/9/25 12:18
+ * @Description interrupt中断
+ */
+public class Thread03Interrupt {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(()->{
+
+        });
+    }
+}
