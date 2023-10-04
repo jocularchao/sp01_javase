58e3792348b1ad708cb720cc06730deed7032bb9
jocularchao <jocularchao@gmail.com>
2023-10-04 19:58:31 +0800

    String new API

---
+++ _08new-features-of-java/day08_jdk11/src/d01lambda/Test.java
@@ -0,0 +1,14 @@
+package d01lambda;
+
+/**
+ * Create with IntelliJ IDEA.
+ *
+ * @author JocularChao
+ * @date 2023/10/4 12:32
+ * @Description
+ */
+@FunctionalInterface
+public interface Test {
+
+      void test(int i);
+}
