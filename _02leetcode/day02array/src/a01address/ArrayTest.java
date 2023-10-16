b32066f5fae6b4297914de999d885f4cb9efd55a
jocularchao <jocularchao@gmail.com>
2023-10-16 18:42:42 +0800

    数组理论基础

---
+++ _02leetcode/day02array/src/a01address/ArrayTest.java
@@ -0,0 +1,16 @@
+package a01address;
+
+/**
+ * Create with IntelliJ IDEA.
+ *
+ * @author JocularChao
+ * @date 2023/10/15 20:39
+ * @Description 数组理论基础
+ */
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[] arr = new int[3];
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+    }
+}
