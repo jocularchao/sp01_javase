6bde485707510dd792fe25699979602d26683910
jocularchao <jocularchao@gmail.com>
2023-10-17 12:12:18 +0800

    研究锁机制

---
+++ _10JUC/day02_synchronized/src/Demo.java
@@ -0,0 +1,18 @@
+/**
+ * Create with IntelliJ IDEA.
+ *
+ * @author JocularChao
+ * @date 2023/10/16 21:26
+ * @Description
+ */
+public class Demo {
+    public static void main(String[] args) {
+
+    }
+
+    public void test() {
+        synchronized (Demo.class){
+
+        }
+    }
+}
